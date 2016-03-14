173eb8eb3a8a1eb049e60d7954a2befe8f8d36c7
Mauricio Machuca <mauricio208@gmail.com>
2016-03-14 15:21:49 -0430

    agregado soporte para jinja2 creada app home!

---
+++ home/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from django.views.generic.base import TemplateView
+
+class HomeView(TemplateView):
+
+    template_name = "home.html"
+
+    def get_context_data(self, **kwargs):
+        context = super(HomeView, self).get_context_data(**kwargs)
+        return context
+
+
+
